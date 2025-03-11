6f8feabe98963d8e664d50405f43b1f8d759d0ef
Yohai Wiener <yohaiwiener@gmail.com>
2025-03-11 12:06:02 +0200

    setup vite

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBOM4Sb3h6aPfXCYWoOIz3GK_yWeX31AaA',
+  authDomain: 'eko-homework-assignment-5.firebaseapp.com',
+  databaseURL: 'https://eko-homework-assignment-5-default-rtdb.firebaseio.com',
+  projectId: 'eko-homework-assignment-5',
+  storageBucket: 'eko-homework-assignment-5.firebasestorage.app',
+  messagingSenderId: '188679171265',
+  appId: '1:188679171265:web:590462f964a0f95fe07840',
+}
+
+const app = initializeApp(firebaseConfig)
+export const db = getDatabase(app)
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+export default {
+  server: {
+    open: true, // automatically open browser on server start
+  },
+}
